21111ba0d5b1ce0e9676223162440fb1b0111ca0
Jefferson  Fernando de Araujo <marmojezz@gmail.com>
2018-10-22 18:23:06 -0300

    Update WeatherObservationStation6.sql

--- 1. Basic Select/WeatherObservationStation6.sql
+++ 1. Basic Select/WeatherObservationStation6.sql
@@ -1,3 +1,5 @@
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
 /*
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
