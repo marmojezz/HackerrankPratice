80a4b389ba2477f704beb19aa92a7b6ae9e6ce40
marmojezz <marmojezz@gmail.com>
2018-11-15 02:24:02 -0200

    test

--- SQL/1. Basic Select/EmployeeNames.sql
+++ SQL/1. Basic Select/EmployeeNames.sql
@@ -1,5 +1,7 @@
 /* Write a query that prints a list of employee names (i.e.: the name attribute) from the 
 Employee table in alphabetical order.*/
 
+-- GitHut
+
 SELECT name FROM Employee ORDER BY name ASC;
 
